1d82ee6d291c8bec5e02e695012bb4b1f00150bb
qasimtahir5611 <qasimtahir5611@gmail.com>
2023-08-31 17:27:14 +0500

    newbot

--- src/pages/ChatbotWidget.js
+++ src/pages/ChatbotWidget.js
@@ -4,7 +4,7 @@ const ChatbotWidget = () => {
   useEffect(() => {
     // Your chatbot widget JavaScript code initialization here
     window.__lc = window.__lc || {};
-    window.__lc.license = 15965643;
+    window.__lc.license = 16007931;
     (function (n, t, c) {
       function i(n) {
         return e._h ? e._h.apply(null, n) : e._q.push(n);
@@ -42,6 +42,17 @@ const ChatbotWidget = () => {
       };
       !n.__lc.asyncInit && e.init(), (n.LiveChatWidget = n.LiveChatWidget || e);
     })(window, document, [].slice);
+
+    const script = document.createElement("script");
+    script.src = "https://unpkg.com/@livechat/customer-sdk@3.1.2";
+    script.async = true;
+    document.body.appendChild(script);
+
+    const script2 = document.createElement("script");
+    script2.src =
+      "https://cdn.livechat-static.com/moments-sdk/moments-sdk-1.8.1.umd.min.js";
+    script2.async = true;
+    document.body.appendChild(script2);
   }, []);
 
   return <div id="chatbot-widget-container"></div>;
--- src/pages/ResultComponent.js
+++ src/pages/ResultComponent.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from "react";
+import axios from "axios";
+import * as CustomerSDK from "@livechat/customer-sdk";
+
+import createMomentsSDK from "@livechat/moments-sdk";
 
 const ResultComponent = (props) => {
   console.log("formData", props);
@@ -12,8 +16,72 @@ const ResultComponent = (props) => {
         travlers: props.formData.travlers,
         class: props.formData.class,
       });
+
+      createMomentsSDK({ title: "My App" }).then((momentsSDK) => {
+        // your code
+        console.log("momentsSDK", momentsSDK);
+      });
+
+      apiCall();
     }
+
+    //////////////////////////////////
   }, [props?.formData]);
+
+  // window.onMomentAsyncInit = function (createMomentsSDK) {
+  //   createMomentsSDK({ title: "My App" }).then(() => {
+  //     // your code
+  //   });
+  // };
+
+  const apiCall = async () => {
+    let headersList = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      Authorization: "Bearer dal:Mn05juPTS3SD3rZL6jwigw",
+      "Content-Type": "application/json",
+    };
+
+    const dynamicText = `
+    Departure: ${props.formData.checkIn}
+    Destination: ${props.formData.checkOut}
+    Depart: ${props.formData.depart}
+    Return: ${props.formData.return}
+    Travelers: ${props.formData.travlers}
+    Class: ${props.formData.class}
+  `;
+
+    let bodyContent = JSON.stringify({
+      chat_id: "S0M4X2BY3Y",
+      event: {
+        type: "message",
+        text: dynamicText,
+        visibility: "all",
+      },
+    });
+
+    let reqOptions = {
+      url: "https://api.livechatinc.com/v3.5/customer/action/send_event?organization_id=56c74d4d-3103-4419-ad82-f4be821870cb",
+      method: "POST",
+      headers: headersList,
+      data: bodyContent,
+    };
+
+    // let response = await axios.request(reqOptions);
+    // console.log(response.data);
+    try {
+      const response = await axios.request(reqOptions);
+      console.log(response.data);
+    } catch (error) {
+      if (error.response) {
+        console.error("API Error:", error.response.data);
+      } else if (error.request) {
+        console.error("Request Error:", error.request);
+      } else {
+        console.error("General Error:", error.message);
+      }
+    }
+  };
 
   return <div id="chatbot-widget-container"></div>;
 };
